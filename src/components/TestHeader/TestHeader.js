3ae4680412dd2a169c38df1a1190de67e709d343
Carlin Mitchell <carlinmi@umn.edu>
2023-08-08 08:50:52 -0500

    “add”

--- src/components/TestHeader/TestHeader.js
+++ src/components/TestHeader/TestHeader.js
@@ -1,6 +1,6 @@
 const TestHeader = (content) => {
   const h1 = Object.assign(document.createElement("h1"), {
-    className: "test-header bg-info p-sm",
+    className: "test-header container text-primary bg-info p-1 mb-1 bw-2 br",
     innerText: `${content}`,
   });
 
